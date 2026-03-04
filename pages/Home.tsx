917761b9749724433f7aa2bceca1a1c383d81407
Autoblogger <dev@famefact.de>
2026-03-04 11:16:38 +0100

    chore: update job listings from external sources (HR-JobScraper)
    25 jobs injected from: Arbeitsagentur, Indeed, StepStone, meinestadt.de

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -15,48 +15,228 @@ import { Search, MapPin, Filter, Settings, Truck } from 'lucide-react';
 const SAMPLE_JOBS: JobListing[] = [
   {
     id: '1',
-    title: 'Rehatechnik-Fachberater im Außendienst (m/w/d)',
-    company: 'Sanitätshaus Müller & Sohn',
-    location: 'Hamburg / Schleswig-Holstein',
+    title: 'Teilbereichsleitung (m/w/d) Innendienst Rehatechnik',
+    company: 'Zimmermann Sanitäts- und Orthopädiehaus GmbH',
+    location: 'Straubing',
     type: 'Vollzeit',
-    tags: ['Aktiv-Reha', 'Außendienst', 'Hilfsmittelberatung'],
-    postedAt: 'Heute'
+    tags: ['Gruppen-, Teamleiter/in'],
+    postedAt: '2026-03-02'
   },
   {
     id: '2',
-    title: 'Orthopädietechnik-Mechaniker / Sonderbau',
-    company: 'Vitalis Reha-Zentrum',
-    location: 'Köln',
+    title: 'Intensivpflegekraft - Beratung / Hausbesuche / Rehatechnik (m/w/d)',
+    company: 'Workwise GmbH',
+    location: 'Dortmund',
     type: 'Vollzeit',
-    tags: ['Sitzschalenbau', 'Sonderbau', 'CAD/CAM'],
-    postedAt: 'Vor 3 Stunden'
+    tags: ['Fachkrankenpfleger/in - Intensivpflege/Anästhesie'],
+    postedAt: '2026-03-02'
   },
   {
     id: '3',
-    title: 'Servicetechniker für Rehatechnik (m/w/d)',
-    company: 'Medizintechnik Nord',
-    location: 'Bremen',
-    type: 'Teilzeit',
-    tags: ['Wartung', 'Rollstühle', 'Führerschein B'],
-    postedAt: 'Vor 2 Tagen'
+    title: 'Außendienstmitarbeiter (m/w/d) –– Vollzeit– Rehatechnik',
+    company: 'SC Sanitätshaus Carstens GmbH',
+    location: 'Stuttgart',
+    type: 'Vollzeit',
+    tags: ['Außendienstmitarbeiter/in'],
+    postedAt: '2026-02-27'
   },
   {
     id: '4',
-    title: 'Fachkraft für Kinder-Rehatechnik',
-    company: 'Kids-Mobility GmbH',
-    location: 'München',
+    title: 'Sachbearbeiter Innendienst Rehatechnik (m/w/d)',
+    company: 'LICURA-Gruppe',
+    location: 'Hessisch Lichtenau',
     type: 'Vollzeit',
-    tags: ['Pädiatrie', 'Sonderbau', 'Einfühlungsvermögen'],
-    postedAt: 'Vor 5 Tagen'
+    tags: ['Kaufmann/-frau - Büromanagement'],
+    postedAt: '2026-02-27'
   },
   {
     id: '5',
-    title: 'Quereinsteiger Hilfsmittel-Logistik',
-    company: 'Logistik-Center Gesundheit',
+    title: 'Servicetechniker*in/Fahrer-in für Rehatechnik (m/w/d)',
+    company: 'Moises Markus',
+    location: 'Ratingen',
+    type: 'Vollzeit',
+    tags: ['Instandhaltungstechniker/in'],
+    postedAt: '2026-02-26'
+  },
+  {
+    id: '6',
+    title: 'Servicetechniker*in/Fahrer-in für Rehatechnik (m/w/d)',
+    company: 'Moises Markus',
+    location: 'Erlangen',
+    type: 'Vollzeit',
+    tags: ['Fachkraft - Möbel-, Küchen- und Umzugsservice'],
+    postedAt: '2026-02-26'
+  },
+  {
+    id: '7',
+    title: 'Außendienstberater (m/w/d) Rehatechnik, LUT - LiBo 55',
+    company: 'Luttermann GmbH',
+    location: 'Essen, Ruhr',
+    type: 'Vollzeit',
+    tags: ['Fachmann/-frau - Rehatechnik/Sanitätshauswaren'],
+    postedAt: '2026-02-25'
+  },
+  {
+    id: '8',
+    title: 'Fahrer/in für Orthopädie-, Rehatechnik gesucht',
+    company: 'Orthopädie-Technik Kächele GmbH',
+    location: 'Stuttgart',
+    type: 'Vollzeit',
+    tags: ['Auslieferungsfahrer/in (nicht Verkaufsfahrer/in)'],
+    postedAt: '2026-02-23'
+  },
+  {
+    id: '9',
+    title: 'Rehatechnik-Berater/-in (m/w/d) in Böblingen',
+    company: 'Schaible GmbH',
+    location: 'Böblingen',
+    type: 'Vollzeit',
+    tags: ['Altenpfleger/in'],
+    postedAt: '2026-02-19'
+  },
+  {
+    id: '10',
+    title: 'Rehatechnik-Berater/-in (m/w/d) in Nagold',
+    company: 'Schaible GmbH',
+    location: 'Nagold',
+    type: 'Vollzeit',
+    tags: ['Altenpfleger/in'],
+    postedAt: '2026-02-19'
+  },
+  {
+    id: '11',
+    title: 'Monteur:in (m/w/d) Rehatechnik Außendienst',
+    company: 'Mitschke Health + Life GmbH',
+    location: 'Gütersloh',
+    type: 'Vollzeit',
+    tags: ['Kundendienstmonteur/in, -techniker/in'],
+    postedAt: '2026-02-18'
+  },
+  {
+    id: '12',
+    title: 'Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik',
+    company: 'Lambert Sanitätshaus GmbH',
+    location: 'Salzburg',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik'],
+    postedAt: '2026-02-17'
+  },
+  {
+    id: '13',
+    title: 'Disponent Rehatechnik (m/w/d)',
+    company: 'Sanitätshaus Reutter Thomas Gockenbach',
+    location: 'Calw',
+    type: 'Vollzeit',
+    tags: ['Fachmann/-frau - Rehatechnik/Sanitätshauswaren'],
+    postedAt: '2026-02-16'
+  },
+  {
+    id: '14',
+    title: 'Mitarbeiter Innendienst Rehatechnik (m/w/d)',
+    company: 'Reha-aktiv GmbH Chemnitz',
+    location: 'Chemnitz, Sachsen',
+    type: 'Vollzeit',
+    tags: ['Kaufmann/-frau - Büromanagement'],
+    postedAt: '2026-02-13'
+  },
+  {
+    id: '15',
+    title: 'Verwaltungsmitarbeiter /-in m/w/d - Rehatechnik',
+    company: 'PerZukunft Arbeitsvermittlung GmbH&Co.KG',
     location: 'Berlin',
     type: 'Vollzeit',
-    tags: ['Auslieferung', 'Montage', 'Patientenkontakt'],
-    postedAt: 'Vor 1 Woche'
+    tags: ['Kaufmann/-frau - Gesundheitswesen'],
+    postedAt: '2026-02-12'
+  },
+  {
+    id: '16',
+    title: 'Mitarbeiter in Rehatechnik',
+    company: 'Elbe-Orthopädie GmbH',
+    location: 'Hemmoor',
+    type: 'Vollzeit',
+    tags: ['Rehabilitationsmitteltechniker/in'],
+    postedAt: '2026-02-02'
+  },
+  {
+    id: '17',
+    title: 'Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik',
+    company: 'Bandagist Heindl GmbH',
+    location: 'Linz',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik'],
+    postedAt: '2026-01-27'
+  },
+  {
+    id: '18',
+    title: 'Tourenplaner*in (m/w/d) für die Rehatechnik',
+    company: 'Orthopädie- und Reha-Technik Wolf GmbH & Co.KG Das Sanitätshaus',
+    location: 'Leipzig',
+    type: 'Vollzeit',
+    tags: ['Kaufmann/-frau - Gesundheitswesen'],
+    postedAt: '2026-01-22'
+  },
+  {
+    id: '19',
+    title: 'Montiererhelfer Rehatechnik (m/w/d)',
+    company: 'S&B Personalservice GmbH',
+    location: 'Seddiner See',
+    type: 'Vollzeit',
+    tags: ['Montagemechaniker/in, Anlagenmonteur/in'],
+    postedAt: '2026-01-18'
+  },
+  {
+    id: '20',
+    title: 'Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik',
+    company: 'Kerkoc GmbH',
+    location: 'Brunn am Gebirge',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik'],
+    postedAt: '2025-12-16'
+  },
+  {
+    id: '21',
+    title: 'Außendienstmitarbeiter (m/w/d) Sitzsonderbau/Rehatechnik',
+    company: 'Stolle Sanitätshaus GmbH & Co. KG',
+    location: 'Schwerin, Mecklenburg',
+    type: 'Vollzeit',
+    tags: ['Rehabilitationsmitteltechniker/in'],
+    postedAt: '2025-12-15'
+  },
+  {
+    id: '22',
+    title: 'Außendienstmitarbeiter (m/w/d) – Medizinprodukte & Rehatechnik',
+    company: 'Sanitätshaus Fuchs & Möller GmbH',
+    location: 'Mannheim',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in'],
+    postedAt: '2025-11-24'
+  },
+  {
+    id: '23',
+    title: 'Bürofachkraft/ Berater/in im Bereich der Rehatechnik (m/w/d)',
+    company: 'Biedermann Orthopädie- Technik GmbH',
+    location: 'Rottweil',
+    type: 'Vollzeit',
+    tags: ['Fachmann/-frau - Rehatechnik/Sanitätshauswaren'],
+    postedAt: '2025-11-05'
+  },
+  {
+    id: '24',
+    title: 'Ausbildung zum Orthopädietechnik-Mechaniker (m/w/d), Schwerpunkt RehaTechnik',
+    company: 'Sanitätshaus Lang GmbH',
+    location: 'Dinslaken',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in'],
+    postedAt: '2025-10-24'
+  },
+  {
+    id: '25',
+    title: 'Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik',
+    company: 'Bandagist Heindl GmbH',
+    location: 'Innsbruck',
+    type: 'Vollzeit',
+    tags: ['Orthopädietechnik-Mechaniker/in - Individuelle Rehatechnik'],
+    postedAt: '2025-10-20'
   }
 ];
 
